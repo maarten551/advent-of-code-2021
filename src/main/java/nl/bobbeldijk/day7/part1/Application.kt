e9602543bd41de7cd85cce4754f8845e0adb7e09
Maarten Bobbeldijk <maarten@bobbeldijk.nl>
2021-12-17 14:05:43 +0100

    Refactored day-7 to work with multi-threading

--- src/main/java/nl/bobbeldijk/day7/part1/Application.kt
+++ src/main/java/nl/bobbeldijk/day7/part1/Application.kt
@@ -1,8 +1,13 @@
 package nl.bobbeldijk.day7.part1
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import nl.bobbeldijk.util.Answerable
 import nl.bobbeldijk.util.InputFile
 import nl.bobbeldijk.util.InputReader
+import kotlin.system.measureTimeMillis
 
 fun main() {
     Application().calculateAnswer(InputReader.readListFromInputFile(InputFile.DAY7))
@@ -10,13 +15,24 @@ fun main() {
 
 open class Application : Answerable<Int> {
     override fun calculateAnswer(input: MutableList<String>): Int {
-        val positions = input[0].split(',').map(String::toInt)
-        val min = positions.minOf { it }
-        val max = positions.maxOf { it }
+        var answer = 0;
 
-        val answer = (min until max).minOf { calculateFuel(positions, it) }
+        val elapsedTime = measureTimeMillis {
+            val positions = input[0].split(',').map(String::toInt)
+            val min = positions.minOf { it }
+            val max = positions.maxOf { it }
+
+            runBlocking {
+                answer = (min until max).map { p ->
+                    async(Dispatchers.Default) {
+                        calculateFuel(positions, p)
+                    }
+                }.awaitAll().minOf { it }
+            }
+        }
+
+        println("Elapsed time: $elapsedTime milliseconds")
         println("Answer: $answer")
-
         return answer
     }
 
